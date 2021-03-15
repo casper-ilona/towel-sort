1d5a44f2c517edef5c6a3030b7e63f177cc0c922
casper-ilona <hair.masterpiece@gmail.com>
2021-03-15 21:25:15 +0300

    решила задачу

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (m) {
+  if (!m) {
+    return [];
+  }
+  return m.reduce ((result,value,index) => {
+    if(index % 2 ) {
+      result.push (...value.reverse());
+    } else {
+      result.push(...value);
+    }
+    return result;
+  }, []);
+  
+  
+};
